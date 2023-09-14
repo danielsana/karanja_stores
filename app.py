d195317b0dba8db4bf8b5383f086d1d992ad9357
user <kbenkamotho.bk@gmail.com>
2023-09-14 15:33:35 +0300

    second commit

--- app.py
+++ app.py
@@ -9,4 +9,34 @@ app = Flask(__name__)
 def main():
     return ('this is my homepage')
 
+@app.route('/vendor_register', methods=['POST','GET'])
+def vendor_Register():
+    if request.method =='POST':
+        vendor_name = request.form['name']
+        vendor_contact = request.form['contact']
+        vendor_email = request.form['email']
+        vendor_location = request.form['location']
+        vendor_password = request.form['password']
+
+        vendor_photo = request.files['photo']
+        vendor_photo.save('static/images/'+vendor_photo.filename)
+
+        vendor_desc = request.form['desc']
+
+        connection = pymysql.connect(host='localhost',user='root',password='',database='karanja_eshop')
+
+        cursor=connection.cursor()
+
+        sql ="insert into vendors (vendor_name,vendor_contact,vendor_email,vendor_location,vendor_password,vendor_photo,vendor_desc) values(%s,%s,%s,%s,%s,%s,%s)"
+
+        cursor.execute(sql,(vendor_name,vendor_contact,vendor_email,vendor_location,vendor_password,vendor_photo.filename,vendor_desc))
+
+        connection.commit()
+
+        return render_template('vendor_register.html', message='vendor successfuly registerd')
+    else:
+        return render_template('vendor_register.html',message='Please Register Here')
+
+
+        
 app.run(debug=True)
